668f09d969ff971a65dbdae753ba95ee694a353f
chuancey <1210043347@qq.com>
2023-01-11 05:00:27 -0500

    删除mysql的依赖

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#include <arpa/inet.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <stdio.h>
-#include <sys/socket.h>
 #include "capture.h"
 #include "checksum.h"
 #include "tcp.h"
@@ -14,25 +9,17 @@
 #define TCP_LEVEL_OFFSET 0x22
 
 int main() {
-    int manage_tcp_stream_nums = 60000;
-    init_hash();
-    init_tcp(manage_tcp_stream_nums);
-    register_tcp_callbk(process_tls);
-    pthread_t tid;
+  int manage_tcp_stream_nums = 60000;
+  init_hash();
+  init_tcp(manage_tcp_stream_nums);
+  register_tcp_callbk(process_tls);
 
-    // if (pthread_create(&tid, NULL, check_sql_server_status, NULL) == -1) {
-    //     fprintf(stderr, "thread create error\n");
-    //     return -1;
-    // }
-    if (caputure_packet_from_file("/home/chuancey/project/src/doc/tls1.2.pcap",
-                                  process_tcp) == -1)
-        return -1;
-    // release_pcap_resource();
-    // release_sql_resource(sql);
-    // release_trie_resource();
-    // while (1) {
-    //     sleep(1);
-    // }
+  if (caputure_packet_from_file(
+          "/home/chuancey/workspace/Protocol_Restoration/doc/tls1.2.pcap",
+          process_tcp) == -1)
+    return -1;
+  release_pcap_resource();
+  // release_trie_resource();
 
-    return 0;
+  return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -2,37 +2,13 @@
 #define __UTILS_H__
 #include <stdint.h>
 
-#include <arpa/inet.h>
-#include <assert.h>
-#include <mysql/mysql.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <time.h>
-
-#define DBHOST "127.0.0.1"
-#define DBPORT 3306
-#define DBUSER "root"
-#define DBPASSWD "Zhangsan@elem123"
-#define DBNAME "FlowCleaning"
-#define SQL_STR_LEN 1024
-
-extern char last_modified_time[50];
-
-extern int connect_to_sql(MYSQL**);
-extern void release_sql_resource(MYSQL*);
-extern int init_last_modified_time(MYSQL*);
-extern int execute_sql_cmd(MYSQL*, const char*);
-extern void* check_sql_server_status(void*);
-
 static inline int before(uint32_t seq1, uint32_t seq2) {
-    return ((int)(seq1 - seq2) < 0);
+  return ((int)(seq1 - seq2) < 0);
 }
 
 static inline int after(uint32_t seq1, uint32_t seq2) {
-    // 不能简单地写成!before，因为存在大于等于的情况
-    return ((int)(seq1 - seq2) > 0);
+  // 不能简单地写成!before，因为存在大于等于的情况
+  return ((int)(seq1 - seq2) > 0);
 }
 
 #endif
